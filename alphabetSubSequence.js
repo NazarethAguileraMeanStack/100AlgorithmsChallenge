f22365542679e539f1de3ebc11cb2b8fdd7b4c33
Nazareth Aguilera <NazarethAguileraMeanStack@gmail.com>
2018-07-02 15:27:51 -0700

    Alphabet Sub Sequence Algorithm

---
+++ alphabetSubSequence.js
@@ -0,0 +1,37 @@
+/*
+8. Check whther the give string is a subsequnce of the plaintext alphabet.
+
+
+example:
+for s = "effg", or s="cdce", the output should be false
+for s = "ace" or s ="bxz", the output should be true
+
+*Basically, check to make sure the letters go in logical alphabetical order
+
+*/
+
+
+
+function alphabetSubSequence(str){
+    let testCase = str.split("").sort();
+    let originalWord = str.split("");
+    
+    for (let i = 0; i < testCase.length; i++){
+        if(testCase[i] == testCase[i + 1]){
+            return false;
+        }
+    }
+
+    if (testCase.join("") === originalWord.join("")){
+        return true;
+    }
+    return false;
+
+
+}
+
+console.log(alphabetSubSequence("effg"));
+console.log(alphabetSubSequence("cdce"));
+console.log(alphabetSubSequence("atce"));
+console.log(alphabetSubSequence("bxz"));
+console.log(alphabetSubSequence("ace"));
