1bef5c9986e3d2511cbd924639463bb7a655d554
Nazareth Aguilera <NazarethAguileraMeanStack@gmail.com>
2018-07-09 11:53:36 -0700

    Enclose In Brackets Algorithm

---
+++ encloseInBrackets.js
@@ -0,0 +1,19 @@
+/*
+45. Enclose In Brackets
+
+-Given a string, enclose it in round brackets.
+
+EXAMPLE:
+string = "abacaba", the output should be "(abacaba)".
+
+*/
+
+function encloseInBrackets(word){
+
+    return "(" + word + ")";
+
+}
+
+console.log(encloseInBrackets("abacaba"));
+console.log(encloseInBrackets("Nazareth"));
+console.log(encloseInBrackets("JavaScript"));
